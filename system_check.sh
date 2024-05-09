81b2cd7546f612247337ccdd1bdd242863ce6f47
Alexis Martin <132392715+AlexisMartinAr@users.noreply.github.com>
2024-05-09 16:28:49 -0300

    add if warnings

--- system_check.sh
+++ system_check.sh
@@ -2,17 +2,13 @@
 # System info
 hostname=$(hostname)
 uptime=$(uptime -p)
-loadavg=$(uptime | awk '{print $NF}')
 cpu_usage=$(top -b -n1 | grep '%Cpu(s):' | awk '{print $2 + $4}')
-free_ram=$(free -m | grep Mem: | awk '{print $4}')
-total_ram=$(free -m | grep Mem: | awk '{print $2}')
+ram_usage=$(free -m | grep Mem: | awk '{print $2}')
 df_output=$(df -h | grep -vE '^Filesystem|tmpfs|udev')
 
 # Sensors info
 CPU_TEMP_FILE=/sys/class/thermal/thermal_zone0/temp
-CPU_TEMP_O=$(cat $CPU_TEMP_FILE)
-CPU_TEMP_C=$((CPU_TEMP_O/1000))
-CPU_TEMP_F=$(($CPU_TEMP_C * 9/5 + 32))
+CPU_TEMP_C=$((cat $CPU_TEMP_FILE)/1000)
 
 # Get thresholds from .conf
 thresholds_file="system_thresholds.conf"
@@ -23,5 +19,11 @@ fi
 
 # Warnings
 if [[ $cpu_usage -ge $CPU_WARNING ]]; then
-   echo "WARNING: CPU usage is at $cpu_usage% (above threshold of $CPU_WARNING%)"
+  echo "WARNING: CPU usage is at $cpu_usage% (above threshold of $CPU_WARNING%)"
+fi
+if [[ $ram_usage -ge $RAM_WARNING ]]; then
+  echo "WARNING: RAM usage is at $ram_usage% (above threshold of $RAM_WARNING%)"
+fi
+if [[ $ ]]
+  echo 
 fi
